1451631e8a67f43e5cd2f2cb2a5c3aeb471b6f20
Andrea Beconcini <andrea.beconcini@gmail.com>
2017-03-19 15:21:27 +0100

    chat db cleaned, removed some imports

--- src/com/alessio/sharenotes/chat/Worker.java
+++ src/com/alessio/sharenotes/chat/Worker.java
@@ -1,12 +1,10 @@
 package com.alessio.sharenotes.chat;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
